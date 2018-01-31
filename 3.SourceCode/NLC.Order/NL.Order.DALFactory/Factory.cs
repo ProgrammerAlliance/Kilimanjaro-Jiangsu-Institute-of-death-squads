e08f06f3b547272b339882eecf32fdc0acd11c57
mmmhong <mh_jsnt@163.com>
2018-01-31 19:18:38 +0800

    1

---
+++ 3.SourceCode/NLC.Order/NL.Order.DALFactory/Factory.cs
@@ -0,0 +1,64 @@
+﻿using NLC.Order.IDAL;
+using NLC.Order.OracleDAL;
+using NLC.Order.SqlServerDAL;
+using System.Configuration;
+
+namespace NLC.Order.DALFactory
+{
+    public class Factory
+    {
+        //获取配置文件中DateBaseType对应的值
+        //private static string dateBaseType = ConfigurationManager.AppSettings["DateBaseType"].ToString();
+        
+        /// <summary>
+        /// 根据配置文件中的值创建UserDAL
+        /// </summary>
+        /// <returns></returns>
+        public static IUserDAL CreateUserDAL()
+        {
+            IUserDAL userDAL = null;
+            //switch (dateBaseType)
+            //{
+            //    case "SqlServer":
+            //        userDAL = new UserSqlServerDAL(); break;
+            //    case "Oracle":
+            //        userDAL = new UserOracleDAL(); break;
+            //}
+            return userDAL;
+        }
+
+        /// <summary>
+        /// 根据配置文件中的值创建OrderDAL
+        /// </summary>
+        /// <returns></returns>
+        public static IUserDAL CreateOrderDAL()
+        {
+            IUserDAL userDAL = null;
+            //switch (dateBaseType)
+            //{
+            //    case "SqlServer":
+            //        userDAL = new UserSqlServerDAL(); break;
+            //    case "Oracle":
+            //        userDAL = new UserOracleDAL(); break;
+            //}
+            return userDAL;
+        }
+
+        /// <summary>
+        /// 根据配置文件中的值创建MenuDAL
+        /// </summary>
+        /// <returns></returns>
+        public static IUserDAL CreateMenuDAL()
+        {
+            IUserDAL userDAL = null;
+            //switch (dateBaseType)
+            //{
+            //    case "SqlServer":
+            //        userDAL = new UserSqlServerDAL(); break;
+            //    case "Oracle":
+            //        userDAL = new UserOracleDAL(); break;
+            //}
+            return userDAL;
+        }
+    }
+}
---
+++ 3.SourceCode/NLC.Order/NLC.Order.SqlServerDAL/MenuSqlServerDAL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NLC.Order.SqlServerDAL
+{
+    class MenuSqlServerDAL
+    {
+    }
+}
