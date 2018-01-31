702c518b568f456daf6abaaa164147b65e337092
mmmhong <mh_jsnt@163.com>
2018-01-31 17:39:13 +0800

    1

---
+++ 3.Sourcecode/NLC.Order/NL.Order.Common/JsonResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NLC.Order.Common
+{
+    public class JsonResult
+    {
+        /// <summary>
+        /// 状态码
+        /// </summary>
+        public int Status { get; set; }
+
+        /// <summary>
+        /// 结果
+        /// </summary>
+        public object Result { get; set; }
+    }
+}
---
+++ 3.Sourcecode/NLC.Order/NLC.Order.OracleDAL/UserOracleDAL.cs
@@ -0,0 +1,61 @@
+﻿using NLC.Order.IDAL;
+using NLC.Order.Model;
+using System;
+using System.Collections.Generic;
+
+namespace NLC.Order.OracleDAL
+{
+    public class UserOracleDAL : IUserDAL
+    {
+        /// <summary>
+        /// 删除用户
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public bool DeleteUser(string userId)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// 插入用户
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public bool InsertUser(UserInfo user)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 查找所有用户
+        /// </summary>
+        /// <returns></returns>
+        public IList<UserInfo> SelectAllUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 根据用户名和密码查找用户
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="pwd"></param>
+        /// <returns></returns>
+        public UserInfo SelectByNameAndPwd(string name, string pwd)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 更新用户
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="psassword"></param>
+        /// <returns></returns>
+        public bool UpdateUser(string userId, string psassword)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
