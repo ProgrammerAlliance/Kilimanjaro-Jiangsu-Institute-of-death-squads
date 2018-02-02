0abeeb5f7b4357d3ce8615415352314cb59b3977
Lizhonngxi <15961170229@163.com>
2018-02-02 10:28:22 +0800

    OrderBLL.cs

--- 3.SourceCode/NLC.Order/NLC.Order.BLL/OrderBLL.cs
+++ 3.SourceCode/NLC.Order/NLC.Order.BLL/OrderBLL.cs
@@ -9,28 +9,70 @@ namespace NLC.Order.BLL
 {
     public class OrderBLL : IOrderBLL
     {
-        private IOrderDAL userDAL = Factory.CreateOrderDAL();
+        private IOrderDAL OrderDAL = Factory.CreateOrderDAL();
         private JsonResult jr = new JsonResult();
-        public JsonResult CancelOrder(int UserId)
+
+        /// <summary>
+        /// 取消订餐
+        /// </summary>
+        /// <param name="UserId"></param>
+        /// <returns></returns>
+        public JsonResult CancelOrder(int  UserId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                jr.Result = OrderDAL.SubOrder(UserId);
+                jr.Status = 200;
+            }
+            catch (Exception)
+            {
+                jr.Status = 500;
+                jr.Result = "系统繁忙";
+            }
+            return jr;
         }
 
+        /// <summary>
+        /// 统计订餐人数
+        /// </summary>
+        /// <returns></returns>
         public JsonResult CountOrderNumber()
         {
-            throw new NotImplementedException();
+            try
+            {
+               
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return null;
         }
 
+        /// <summary>
+        /// 获得订餐人员信息
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
         public JsonResult GetOrderPeople(OrderInfo order)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 订餐
+        /// </summary>
+        /// <returns></returns>
         public JsonResult OrderFood()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 生成打扫
+        /// </summary>
+        /// <returns></returns>
         public JsonResult ProudceSweep()
         {
             throw new NotImplementedException();
