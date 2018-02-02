9c717bc9686697b541c8c1424255670511ccfad2
weiyufeng <weiyufeng@aecg.com.cn>
2018-02-02 10:54:08 +0800

    修改order对象

--- 3.SourceCode/NLC.Order/NLC.Order.Model/OrderInfo.cs
+++ 3.SourceCode/NLC.Order/NLC.Order.Model/OrderInfo.cs
@@ -12,22 +12,36 @@ namespace NLC.Order.Model
         /// 订餐号
         /// </summary>
         public int OrderNo { get; set; }
+
         /// <summary>
         /// 员工编号
         /// </summary>
         public int UserId { get; set; }
+
         /// <summary>
         /// 点餐时间
         /// </summary>
         public DateTime CreateTime { get; set; }
+
         /// <summary>
         /// 打扫人员
         /// </summary>
         public int Clean { get; set; }
+
         /// <summary>
         /// 点餐备注
         /// </summary>
         public string Remark { get; set; }
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 部门名
+        /// </summary>
+        public string DeptName { get; set; }
 
     }
 }
