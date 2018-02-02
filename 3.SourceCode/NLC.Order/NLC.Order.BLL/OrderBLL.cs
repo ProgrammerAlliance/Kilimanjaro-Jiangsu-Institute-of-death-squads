b742fc2a3e84cfd5187f3517f182467a74879865
Lizhonngxi <15961170229@163.com>
2018-02-02 10:03:45 +0800

    OrderBLL.cs

--- 3.SourceCode/NLC.Order/NLC.Order.BLL/OrderBLL.cs
+++ 3.SourceCode/NLC.Order/NLC.Order.BLL/OrderBLL.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NLC.Order.IBLL;
+using System;
+using NLC.Order.Common;
+using NLC.Order.Model;
+using NLC.Order.IDAL;
+using NLC.Order.DALFactory;
 
 namespace NLC.Order.BLL
 {
-    class OrderBLL
+    public class OrderBLL : IOrderBLL
     {
+        private IOrderDAL userDAL = Factory.CreateOrderDAL();
+        private JsonResult jr = new JsonResult();
+        public JsonResult CancelOrder(int UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public JsonResult CountOrderNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        public JsonResult GetOrderPeople(OrderInfo order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public JsonResult OrderFood()
+        {
+            throw new NotImplementedException();
+        }
+
+        public JsonResult ProudceSweep()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
