5aea797daf6242515836ae75150302ae4cc860af
weiyufeng <weiyufeng@aecg.com.cn>
2018-02-02 11:10:02 +0800

    1

--- 3.SourceCode/NLC.Order/NLC.Order.SqlServerDAL/OrderSqlServerDAL.cs
+++ 3.SourceCode/NLC.Order/NLC.Order.SqlServerDAL/OrderSqlServerDAL.cs
@@ -62,7 +62,9 @@ namespace NLC.Order.SqlServerDAL
         /// <returns></returns>
         public List<OrderInfo> Cleaner()
         {
-            string sql = "select * from OrderTable where DateDiff(dd,createtime,getdate())=0";
+            string sql = "select o.UserId,e.UserName,d.Deptname,o.Remark " +
+                "from OrderTable o,Emp e, Deptment d " +
+                "where o.userid = e.userid and e.deptno = d.deptno and DateDiff(dd, createtime, getdate())= 0";
             DataSet ds = DBHelper.Query(sql, null);
             return DBHelper.GetListbyDataSet<OrderInfo>(ds);
         }
@@ -84,7 +86,20 @@ namespace NLC.Order.SqlServerDAL
         /// <returns></returns>
         public bool ModifyCleanState(int UserId)
         {
-            throw new NotImplementedException();
+            string sql = "update OrderTable set Clean=1 where UserId=@UserId";
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("UserId",UserId)
+            };
+            int result = DBHelper.NonQuery(sql, parameters);
+            if (result > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
     }
