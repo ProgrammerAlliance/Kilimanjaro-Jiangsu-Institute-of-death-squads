ea9e7cfbc647b369ee3092a643332ba5bf243091
Lizhonngxi <15961170229@163.com>
2018-02-03 14:26:21 +0800

    Menu实体

--- 3.SourceCode/NLC.Order/NLC.Order.Model/MenuInfo.cs
+++ 3.SourceCode/NLC.Order/NLC.Order.Model/MenuInfo.cs
@@ -8,5 +8,24 @@ namespace NLC.Order.Model
 {
     class MenuInfo
     {
+        /// <summary>
+        /// 菜编号
+        /// </summary>
+        public int DishNumber { get; set; }
+
+        /// <summary>
+        /// 菜名
+        /// </summary>
+        public int DishName { get; set; }
+
+        /// <summary>
+        /// 菜图片路径
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// 价格
+        /// </summary>
+        public int Price { get; set; }
     }
 }
