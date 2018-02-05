225fb0decbc2ffb15910ddf97bb9260e24022412
mmmhong <mh_jsnt@163.com>
2018-02-05 16:03:16 +0800

    添加部门Controller,DAL,BLL

---
+++ 3.SourceCode/NLC.Order/NLC.Order.Model/DeptInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLC.Order.Model
+{
+    public class DeptInfo
+    {
+        /// <summary>
+        /// 员工编号
+        /// </summary>
+        public int DeptNo { get; set; }
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string DeptName { get; set; }
+    }
+}
---
+++ 3.SourceCode/NLC.Order/NLC.Order.WebApi/Controllers/DeptController.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using NLC.Order.BLL;
+using NLC.Order.Common;
+using NLC.Order.IBLL;
+using NLC.Order.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NLC.Order.WebApi.Controllers
+{
+    public class DeptController : ApiController
+    {
+        /// <summary>
+        /// DeptBLL对象
+        /// </summary>
+        private IDeptBLL deptBLL = new DeptBLL();
+
+        /// <summary>
+        /// 获取所有部门信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public JsonResult GetAllDept()
+        {
+            return deptBLL.GetAllDept();
+        }
+    }
+}
