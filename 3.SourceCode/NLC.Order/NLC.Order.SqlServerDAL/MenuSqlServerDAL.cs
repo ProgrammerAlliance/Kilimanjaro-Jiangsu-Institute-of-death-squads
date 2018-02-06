ecd96d968a8f618d636753a28cb8b8a4515013f0
weiyufeng <weiyufeng@aecg.com.cn>
2018-02-06 18:04:16 +0800

    添加菜单方法

--- 3.SourceCode/NLC.Order/NLC.Order.SqlServerDAL/MenuSqlServerDAL.cs
+++ 3.SourceCode/NLC.Order/NLC.Order.SqlServerDAL/MenuSqlServerDAL.cs
@@ -7,16 +7,40 @@ namespace NLC.Order.SqlServerDAL
 {
     public class MenuSqlServerDAL : IMenuDAL
     {
+        /// <summary>
+        /// 删除菜单
+        /// </summary>
+        /// <param name="menuId"></param>
+        /// <returns></returns>
         public bool DelMenu(int menuId)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 插入菜单
+        /// </summary>
+        /// <param name="menu"></param>
+        /// <returns></returns>
         public bool InsertMenu(MenuInfo menu)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 查找所有饭店
+        /// </summary>
+        /// <returns></returns>
+        public List<RestaurInfo> SelectAllRestaurant()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 根据饭店查找所有菜单
+        /// </summary>
+        /// <param name="restaurantId"></param>
+        /// <returns></returns>
         public List<MenuInfo> SelectMenuByRestaurant(int restaurantId)
         {
             throw new NotImplementedException();
