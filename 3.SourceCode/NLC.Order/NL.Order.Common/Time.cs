6f9c79d5c24e4ed17f770736fd9f645ffffadaf4
mmmhong <mh_jsnt@163.com>
2018-02-08 11:31:54 +0800

    1

---
+++ 3.SourceCode/NLC.Order/NL.Order.Common/Time.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+
+namespace NLC.Order.Common
+{
+    public class Time
+    {
+        /// <summary>
+        /// 是否已到截止时间
+        /// </summary>
+        /// <returns></returns>
+        public bool IsTimeUp()
+        {
+            System.DateTime currentTime = System.DateTime.Now;
+
+            int hour = Convert.ToInt32(ConfigurationManager.AppSettings["Hour"]);
+            int minute = Convert.ToInt32(ConfigurationManager.AppSettings["Minute"]);
+
+            if (currentTime.Hour < hour || (currentTime.Hour <= hour && currentTime.Minute <= minute))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
